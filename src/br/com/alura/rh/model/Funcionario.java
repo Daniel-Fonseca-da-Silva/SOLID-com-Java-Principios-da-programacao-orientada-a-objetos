130f962f7c5721315231018e45c2ae856d891b8c
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-04 02:21:09 -0300

    Implementando uma nova rega de negócio

--- src/br/com/alura/rh/model/Funcionario.java
+++ src/br/com/alura/rh/model/Funcionario.java
@@ -66,4 +66,7 @@ public class Funcionario {
 		this.dataUltimoReajuste = dataUltimoReajuste;
 	}
 
+    public void promover(Cargo novoCargo) {
+		this.cargo = novoCargo;
+    }
 }
---
+++ src/br/com/alura/rh/service/promocao/PromocaoService.java
@@ -0,0 +1,21 @@
+package br.com.alura.rh.service.promocao;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Cargo;
+import br.com.alura.rh.model.Funcionario;
+
+public class PromocaoService {
+
+    public void promover(Funcionario funcionario, boolean metaBatida) {
+        Cargo cargoAtual = funcionario.getCargo();
+        if(Cargo.GERENTE == cargoAtual) {
+            throw new ValidacaoException("Gerente não podem ser promovidos!");
+        }
+        if(metaBatida) {
+            Cargo novoCargo = cargoAtual.getProximoCargo();
+            funcionario.promover(novoCargo);
+        } else {
+            throw new ValidacaoException("Funcionário não conseguiu bater a meta!");
+        }
+    }
+}
